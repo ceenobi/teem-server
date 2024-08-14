90168b9de27cd716593fe94821a0714898e3feb1
ceenobi <cobimbachu@gmail.com>
2024-08-14 15:02:18 +0100

    cors added

--- src/app.js
+++ src/app.js
@@ -25,7 +25,12 @@ cloudinary.config({
   secure: true,
 });
 const corsOptions = {
-  origin: ["http://localhost:5174", "http://localhost:5173"], // Allows requests from specified domains. Specify actual domain in production for security.
+  origin: [
+    "https://teemclient.vercel.app/",
+    "https://teem-seller.vercel.app/",
+    "http://localhost:5173",
+    "http://localhost:5174",
+  ], // Allows requests from specified domains. Specify actual domain in production for security.
   optionsSuccessStatus: 200, // Ensure compatibility by setting OPTIONS success status to 200 OK.
   methods: ["GET", "POST", "PATCH", "DELETE"], // Allowed method
 };
